b8147e8d5cebd3638c94ec051bc784222c157dea
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-09-09 08:56:56 +0300

    brigtness chnage

---
+++ brightness_change/brightness_change.ino
@@ -0,0 +1,39 @@
+int pin = 19;
+int brightness = 0;
+int step = 1;
+
+void setup() {
+  pinMode(pin, OUTPUT);
+}
+
+void loop() {
+  digital_Write(pin, brightness);
+  brightness_changer();
+}
+
+void digital_Write(int pin, int level) {
+  const int period = 255;
+  if (level < 0){
+    level = 0;
+  }
+  if (level > 255) {
+    level = 255;
+  }
+  
+  for (int i = 0; i < period; i++) {
+    if (i < level) {
+      digitalWrite(pin, HIGH);
+    } else {
+      digitalWrite(pin, LOW);
+    }
+    delayMicroseconds(50);aa
+  }
+}
+
+void brightness_changer() {
+  brightness += step;
+  if (brightness >= 255 || brightness <= 0) {
+    step = -step;
+  }
+  delay(20);
+}
